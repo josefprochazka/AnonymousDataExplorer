f7d5917e4ab4c32cdcba515edb39f99d7952bc7e
Josef Procházka <josef.prochazka@bcpraha.com>
2025-07-18 09:47:03 +0200

    First commit - basic thing

---
+++ AnonymousDataExplorer/Program.cs
@@ -0,0 +1,28 @@
+using AnonymousDataExplorer.Components;
+using AnonymousDataExplorer.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<DatabaseService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ AnonymousDataExplorer/Services/DatabaseService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace AnonymousDataExplorer.Services
+{
+	public class DatabaseService
+	{
+		private readonly string _connectionString;
+
+		public DatabaseService(IConfiguration configuration)
+		{
+			_connectionString = configuration.GetConnectionString("Default");
+		}
+
+		public async Task<List<string>> GetTableNamesAsync()
+		{
+			var result = new List<string>();
+
+			using var connection = new SqliteConnection(_connectionString);
+			await connection.OpenAsync();
+
+			var command = new SqliteCommand(
+				   "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%';",
+				   connection);
+
+			using var reader = await command.ExecuteReaderAsync();
+			while (await reader.ReadAsync())
+			{
+				result.Add(reader.GetString(0));
+			}
+
+			return result;
+		}
+	}
+}
